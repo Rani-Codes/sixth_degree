932038be58af83b77db5099a9b4f88078cd266c3
Rani-Codes <92886322+Rani-Codes@users.noreply.github.com>
2025-08-15 22:08:04 -0700

    created worker structs and initialize them

--- internal/fetcher/worker.go
+++ internal/fetcher/worker.go
@@ -1,1 +1,45 @@
 package fetcher
+
+import (
+	"time"
+)
+
+type JobRequest struct {
+	Name string
+	ID   int //Tracks progress
+}
+
+type JobResult struct {
+	Name        string
+	Connections []string
+	Error       error
+	ID          int
+}
+
+type WorkerPool struct {
+	numWorkers int
+	rateLimit  time.Duration
+	validNames map[string]bool
+	jobs       chan JobRequest
+	results    chan JobResult
+	done       chan bool
+}
+
+//Producer Goroutine(1)
+//Must haves:
+// ├── Read unique_names.txt line by line
+// ├── Create JobRequest for each name
+// ├── Send to jobs channel
+// └── Close jobs channel when done
+
+// Constructor that initializes WorkerPool struct
+func NewWorkerPool(numWorkers int, rateLimit time.Duration, validNames map[string]bool) *WorkerPool {
+	return &WorkerPool{
+		numWorkers: numWorkers,
+		rateLimit:  rateLimit,
+		validNames: validNames,
+		jobs:       make(chan JobRequest, 100),
+		results:    make(chan JobResult, 100),
+		done:       make(chan bool),
+	}
+}
