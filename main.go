2caa98149381c84a2d688b195aab03a2eb323750
Rani-Codes <92886322+Rani-Codes@users.noreply.github.com>
2025-08-16 23:55:56 -0700

    add concurrency logic to main, connect everything

--- main.go
+++ main.go
@@ -1,27 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"log"
-
 	"github.com/Rani-Codes/sixth_degree/internal/fetcher"
 )
 
 func main() {
-	// Simple fetchAllLinks test
-	pageTitle := "United States"
+	validNames := fetcher.LoadValidNames("seed_names.txt")
+	pool := fetcher.NewWorkerPool(3, validNames)
+	pool.Run("seed_names.txt")
 
-	links, err := fetcher.FetchAllLinks(pageTitle)
-	if err != nil {
-		log.Fatal("Error fetching links:", err)
-	}
-
-	fmt.Printf("Found %d links for page %q:\n", len(links), pageTitle)
-
-	for i, link := range links {
-		if i >= 10 {
-			break
-		}
-		fmt.Println(link)
-	}
 }
