6aeda008b1a95221383d9da903b366555c659b07
Rani-Codes <92886322+Rani-Codes@users.noreply.github.com>
2025-08-19 18:46:42 -0700

    Created BFS models

--- models/graph.go
+++ models/graph.go
@@ -1,3 +1,18 @@
 package models
 
-// graph data structure model
+// graph data structure models and type alias
+type Graph map[string][]string
+
+type PathRequest struct {
+	StartNode string `json:"startNode"`
+	EndNode   string `json:"endNode"`
+}
+
+type PathResponse struct {
+	Path      []string `json:"path,omitempty"` // the actual path
+	Length    int      `json:"length"`         // path length (0 if no path)
+	Found     bool     `json:"found"`
+	StartNode string   `json:"startNode"`
+	EndNode   string   `json:"endNode"`
+	Error     string   `json:"error,omitempty"`
+}
