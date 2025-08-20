02fba46dacb5fb3b36260299157faaa4e729c7d0
Rani-Codes <92886322+Rani-Codes@users.noreply.github.com>
2025-08-19 18:47:15 -0700

    Implement the LoadGraph function

--- cmd/search/main.go
+++ cmd/search/main.go
@@ -1,3 +1,17 @@
-package search
+package main
+
+import (
+	"log"
+
+	"github.com/Rani-Codes/sixth_degree/internal/graph"
+)
 
 // BFS & Websocket server here
+func main() {
+	g, err := graph.LoadGraph("graph.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("Loaded graph with %d nodes\n", len(*g))
+}
--- internal/graph/loader.go
+++ internal/graph/loader.go
@@ -1,3 +1,28 @@
 package graph
 
+import (
+	"encoding/json"
+	"log"
+	"os"
+
+	"github.com/Rani-Codes/sixth_degree/models"
+)
+
 // Load graph.json into memory
+func LoadGraph(filename string) (*models.Graph, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		// Stops program because if no graph then no BFS and no app
+		log.Fatal("Failed to open file to load graph. ", err)
+	}
+	defer file.Close()
+
+	var graph models.Graph
+	if err := json.NewDecoder(file).Decode(&graph); err != nil {
+		// No graph then no BFS... have to log.Fatal
+		log.Fatal("Failed to decode graph file into graph type, (breaks BFS). ", err)
+	}
+
+	return &graph, nil
+
+}
