b5842eae86fb645d2cdb13d7bbb17ef6d545b91b
Rani-Codes <92886322+Rani-Codes@users.noreply.github.com>
2025-08-29 18:42:48 +0300

    update bfs for websocket streaming

--- internal/graph/bfs.go
+++ internal/graph/bfs.go
@@ -7,10 +7,11 @@ import (
 )
 
 // BFS algorithm
-func FindShortestPath(graph models.Graph, startNode, endNode string) ([]string, error) {
+func FindShortestPath(graph models.Graph, startNode, endNode string, updateCallback func(level int, node string)) ([]string, error) {
 	queue := []string{startNode}
 	parent := make(map[string]string)           // using make to get an empty map (to be filled later)
 	visited := map[string]bool{startNode: true} // literal definition since we have default content
+	level := 0
 
 	// New concept learned, Go’s comma-ok idiom (useful for safe lookup on maps)
 	//	ok returns true if the key exists in the map otherwise exits with error of what went wrong
@@ -22,39 +23,41 @@ func FindShortestPath(graph models.Graph, startNode, endNode string) ([]string, 
 		return nil, fmt.Errorf("end node %q not found in graph", endNode)
 	}
 
+	// BFS loop
 	for len(queue) > 0 {
-		current := queue[0]
-		queue = queue[1:]
+		levelSize := len(queue)
+		for i := 0; i < levelSize; i++ {
+			current := queue[0]
+			queue = queue[1:]
 
-		if current == endNode {
-			break
-		}
-
-		if !visited[current] {
-			visited[current] = true
-		}
-
-		for _, neighbor := range graph[current] {
-			if !visited[neighbor] {
-				visited[neighbor] = true
-				parent[neighbor] = current
-				queue = append(queue, neighbor)
+			if updateCallback != nil {
+				updateCallback(level, current)
 			}
+
+			if current == endNode {
+				// Recreates path
+				path := []string{} // creates an empty slice that is not nil (var path []string would create nil till append)
+				for at := endNode; at != ""; at = parent[at] {
+					path = append([]string{at}, path...) // prepends name to path
+					if at == startNode {
+						break
+					}
+				}
+				if len(path) == 0 || path[0] != startNode {
+					return nil, fmt.Errorf("no path from %s node to %s", startNode, endNode)
+				}
+				return path, nil
+			}
+			for _, neighbor := range graph[current] {
+				if !visited[neighbor] {
+					visited[neighbor] = true
+					parent[neighbor] = current
+					queue = append(queue, neighbor)
+				}
+			}
+
 		}
+		level++
 	}
-
-	// Recreates path from parent map
-	path := []string{} // creates an empty slice that is not nil (var path []string would create nil till append)
-	for at := endNode; at != ""; at = parent[at] {
-		path = append([]string{at}, path...) // prepends name to path
-		if at == startNode {
-			break
-		}
-	}
-
-	if len(path) == 0 || path[0] != startNode {
-		return nil, fmt.Errorf("no path from %s node to %s", startNode, endNode)
-	}
-
-	return path, nil
+	return nil, fmt.Errorf("no path from %s to %s", startNode, endNode)
 }
