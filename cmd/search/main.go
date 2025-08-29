f3b712c7ea3f6a1dcbc2df3c55b5f08ae2170383
Rani-Codes <92886322+Rani-Codes@users.noreply.github.com>
2025-08-29 18:41:38 +0300

    begin work on websocket connection

--- cmd/search/main.go
+++ cmd/search/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"strings"
 
 	"github.com/Rani-Codes/sixth_degree/internal/graph"
+	"github.com/Rani-Codes/sixth_degree/models"
+	"github.com/gorilla/websocket"
 )
 
 // BFS & Websocket server here
@@ -17,10 +20,39 @@ func main() {
 	log.Printf("Loaded graph with %d nodes\n", len(*g))
 
 	// Test a path
-	path, err := graph.FindShortestPath(*g, "Alexander the Great", "Taylor Swift")
+	path, err := graph.FindShortestPath(*g, "Jesus", "Cristiano Ronaldo", nil)
 	if err != nil {
 		log.Printf("Error: %v", err)
 	} else {
 		log.Printf("Path found: %s (length: %d)", strings.Join(path, " -> "), len(path))
+	}
+
+	// Register WebSocket handler for /ws endpoint
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		handleWebSocket(w, r, *g)
+	})
+
+	// Start HTTP server
+	log.Println("WebSocket server starting on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true // Allowing all origins for now (change in prod)
+	},
+}
+
+func handleWebSocket(w http.ResponseWriter, r *http.Request, graph models.Graph) {
+	// Upgrades the HTTP server connection to the WebSocket protocol.
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Printf("WebSocket upgrade failed: %v", err)
+		return
+	}
+	defer conn.Close()
+
+	for {
+
 	}
 }
