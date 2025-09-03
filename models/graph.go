e9a158b9bda4b5c93c687f76739a13d088611579
Rani-Codes <92886322+Rani-Codes@users.noreply.github.com>
2025-09-03 12:54:47 -0700

    update models to work w/websockets

--- models/graph.go
+++ models/graph.go
@@ -3,16 +3,37 @@ package models
 // graph data structure models and type alias
 type Graph map[string][]string
 
-type PathRequest struct {
+/*
+Datapipeline between BFS and Websocket connection
+Client sends:
+{"startNode": "Einstein", "endNode": "Newton"}
+
+Server streams back:
+{"type": "node_explored", "data": {"level": 1, "node": "Tesla"}}
+{"type": "path_found", "data": {"path": ["Einstein", "Tesla", "Newton"], "length": 3}}
+*/
+
+// Websocket communication
+type WSRequest struct {
 	StartNode string `json:"startNode"`
 	EndNode   string `json:"endNode"`
 }
 
-type PathResponse struct {
-	Path      []string `json:"path,omitempty"` // the actual path
-	Length    int      `json:"length"`         // path length (0 if no path)
-	Found     bool     `json:"found"`
-	StartNode string   `json:"startNode"`
-	EndNode   string   `json:"endNode"`
-	Error     string   `json:"error,omitempty"`
+type WSResponse struct {
+	Type string `json:"type"` // Type field lets us know what kind of response is in data
+
+	// Using interface here cuz then each message can have different payload structure
+	Data interface{} `json:"data"` // Data interface can receive NodeExplored, PathFound, or Error responses
+	// This lets us use one response struct for all message types instead of separate structs
+}
+
+// Response payloads
+type NodeExplored struct {
+	Level int    `json:"level"`
+	Node  string `json:"node"`
+}
+
+type PathFound struct {
+	Path   []string `json:"path"`
+	Length int      `json:"length"`
 }
